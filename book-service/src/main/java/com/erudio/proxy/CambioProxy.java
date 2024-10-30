5f786d8922fd0e3c547cd44b08d09fcaca7c8639
FelipeGabrill <felipegabrielodss@gmail.com>
2024-10-30 18:23:58 -0300

    Criando uma Aplicação Service Discovery/Registy com Eureka

--- book-service/src/main/java/com/erudio/proxy/CambioProxy.java
+++ book-service/src/main/java/com/erudio/proxy/CambioProxy.java
@@ -1,7 +1,5 @@
 package com.erudio.proxy;
 
-import java.math.BigDecimal;
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
